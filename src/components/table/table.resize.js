7dc15ddb79e8aebc4a1842817ff5cc7e948d1bbb
Ogonick <ogonick1@gmail.com>
2023-03-07 11:59:50 +0200

    add store, custom redux

--- src/components/table/table.resize.js
+++ src/components/table/table.resize.js
@@ -1,44 +1,54 @@
 import { $ } from '../../core/Dom';
 
-export function resizesTable($root, event) {
-  const $resizes = $(event.target);
-  const $parent = $resizes.closest('[data-type="resizes"]');
-  const coords = $parent.getCoords();
-  const type = $resizes.data.resize;
-  const sideProp = type === 'col' ? 'bottom' : 'right';
-  let value;
-  $resizes.css({
-    opacity: 1,
-    [sideProp]: '-3000px',
-  });
-  document.onmousemove = (e) => {
-    if (type === 'col') {
-      const delta = e.pageX - coords.right;
-      value = coords.width + delta;
-      $resizes.css({ right: `${-delta}px` });
-    } else {
-      const delta = e.pageY - coords.bottom;
-      value = coords.height + delta;
-      $resizes.css({ bottom: `${-delta}px` });
-    }
-  };
+export function resizeHandler($root, event) {
+  return new Promise((resolve) => {
+    const $resized = $(event.target);
+    const $parent = $resized.closest('[data-type="resizable"]');
+    const coords = $parent.getCoords();
+    const type = $resized.data.resize;
+    const sideProp = type === 'col' ? 'bottom' : 'right';
+    let value;
 
-  document.onmouseup = () => {
-    document.onmousemove = null;
-    document.onmouseup = null;
-
-    if (type === 'col') {
-      $parent.css({ width: `${value}px` });
-      $root.findAll(`[data-col="${$parent.data.col}"]`)
-        .forEach((el) => el.style.width = `${value}px`);
-    } else {
-      $parent.css({ height: `${value}px` });
-    }
-
-    $resizes.css({
-      opacity: 0,
-      bottom: 0,
-      right: 0,
+    $resized.css({
+      opacity: 1,
+      [sideProp]: '-5000px',
     });
-  };
+
+    document.onmousemove = (e) => {
+      if (type === 'col') {
+        const delta = e.pageX - coords.right;
+        value = coords.width + delta;
+        $resized.css({ right: `${-delta}px` });
+      } else {
+        const delta = e.pageY - coords.bottom;
+        value = coords.height + delta;
+        $resized.css({ bottom: `${-delta}px` });
+      }
+    };
+
+    document.onmouseup = () => {
+      document.onmousemove = null;
+      document.onmouseup = null;
+
+      if (type === 'col') {
+        $parent.css({ width: `${value}px` });
+        $root.findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach((el) => el.style.width = `${value}px`);
+      } else {
+        $parent.css({ height: `${value}px` });
+      }
+
+      resolve({
+        value,
+        type,
+        id: $parent.data[type],
+      });
+
+      $resized.css({
+        opacity: 0,
+        bottom: 0,
+        right: 0,
+      });
+    };
+  });
 }
